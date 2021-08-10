bbb2c7d4a1c506fa717e21a4cc76319dc0e54f2b
Luciano Silveira <ladrians@gmail.com>
2021-08-10 17:21:46 -0300

    remove TODOs

--- include/process.h
+++ include/process.h
@@ -10,12 +10,12 @@ class Process {
  public:
   Process(int id) : pid_(id) {};
   int Pid();
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  std::string User();
+  std::string Command();
+  float CpuUtilization();
+  std::string Ram();
+  long int UpTime();
+  bool operator<(Process const& a) const;
 
  private:
   int pid_{0};
